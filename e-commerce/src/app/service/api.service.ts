e6a3c75a5c7ec7aa2bb4d48c6a4ebeef50265907
gabii6431 <gabii6431@gmail.com>
2019-11-19 15:20:15 -0300

    Model, api.service Produto

--- e-commerce/src/app/service/api.service.ts
+++ e-commerce/src/app/service/api.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Produto } from 'src/model/produto';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = 'http://localhost:5000/produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProdutos (): Observable<Produto[]> {
+    return this.http.get<Produto[]>(apiUrl)
+      .pipe(
+        tap(produtos => console.log('leu os produtos')),
+        catchError(this.handleError('getProdutos', []))
+      );
+  }
+
+  getProduto(id: number): Observable<Produto> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.get<Produto>(url).pipe(
+      tap(_ => console.log(`leu o produto id=${id}`)),
+      catchError(this.handleError<Produto>(`getProduto id=${id}`))
+    );
+  }
+
+  addProduto (produto): Observable<Produto> {
+    return this.http.post<Produto>(apiUrl, produto, httpOptions).pipe(
+      // tslint:disable-next-line:no-shadowed-variable
+      tap((produto: Produto) => console.log(`adicionou o produto com w/ id=${produto._id}`)),
+      catchError(this.handleError<Produto>('addProduto'))
+    );
+  }
+
+  updateProduto(id, produto): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.put(url, produto, httpOptions).pipe(
+      tap(_ => console.log(`atualiza o produco com id=${id}`)),
+      catchError(this.handleError<any>('updateProduto'))
+    );
+  }
+
+  deleteProduto (id): Observable<Produto> {
+    const url = `${apiUrl}/delete/${id}`;
+
+    return this.http.delete<Produto>(url, httpOptions).pipe(
+      tap(_ => console.log(`remove o produto com id=${id}`)),
+      catchError(this.handleError<Produto>('deleteProduto'))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+
+      return of(result as T);
+    };
+  }
 }
---
+++ e-commerce/src/model/produto.ts
@@ -0,0 +1,10 @@
+export class Produto{
+    _id: string;
+    titulo: string;
+    descricao: string;
+    preco: string;
+    qtdP: string;
+    qtdM: string;
+    qtdG: string;
+    categoria: string;
+}
