1e669ef41a00e7332efd820b87b6ee1319ee599b
gabii6431 <gabii6431@gmail.com>
2019-11-23 15:17:46 -0300

    Armazenando Produto com suas respectivas fotos

--- e-commerce/src/app/login/login.component.ts
+++ e-commerce/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
     selector: 'app-login',
@@ -8,28 +7,28 @@ import { AuthenticationService } from '../service/authentication.service';
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    email: string;
-    hide = true;
-    password: string;
-    title = "auth-guard-demo";
+    // email: string;
+    // hide = true;
+    // password: string;
+    // title = "auth-guard-demo";
 
-    constructor(private _auth: AuthenticationService, private _router: Router) {
-        if (this._auth.loggedIn) {
-            this._router.navigate(['home']);
-        }
-    }
+    // constructor(private _router: Router) {
+    //     if (this._auth.loggedIn) {
+    //         this._router.navigate(['home']);
+    //     }
+    // }
 
     ngOnInit() {
     }
 
-    login(): void {
-        if (this.email != '' && this.password != '') {
-            if (this._auth.login(this.email, this.password)) {
-                this._router.navigate(["home"]);
-            } else {
-                alert("Wrong username or password");
-            }
-        }
-    }
+    // login(): void {
+    //     if (this.email != '' && this.password != '') {
+    //         if (this._auth.login(this.email, this.password)) {
+    //             this._router.navigate(["home"]);
+    //         } else {
+    //             alert("Wrong username or password");
+    //         }
+    //     }
+    // }
 
 }
--- e-commerce/src/model/produto.ts
+++ e-commerce/src/model/produto.ts
@@ -1,10 +1,12 @@
+import { Fotos } from 'src/model/fotos';
 export class Produto{
-    _id: string;
+    _id: number;
     titulo: string;
     descricao: string;
     preco: string;
     qtdP: number;
     qtdM: number;
     qtdG: number;
+    fotos: Fotos = new Fotos();
     categoria: string;
 }
