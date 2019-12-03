7b5c8a81d4d9884519469547e0cdc5da4ba23d3e
gabii6431 <gabii6431@gmail.com>
2019-12-03 14:37:03 -0300

    Add no carrinho apenas se Logado

--- e-commerce/src/app/produtos/produtos.component.ts
+++ e-commerce/src/app/produtos/produtos.component.ts
@@ -5,6 +5,7 @@ import { ProdutoService } from '../service/produto.service';
 import { Produto } from '../../model/produto';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
@@ -32,7 +33,7 @@ export class ProdutosComponent implements OnInit {
     'qtdG': new FormControl(null, [Validators.required]),
   });
 
-  constructor(private modalService: BsModalService, private produtoService : ProdutoService, private route: ActivatedRoute, private sanitizer : DomSanitizer) { }
+  constructor(private router: Router, private modalService: BsModalService, private produtoService : ProdutoService, private route: ActivatedRoute, private sanitizer : DomSanitizer) { }
 
   ngOnInit(){
     this.route.params.subscribe((parametro: any) =>{
@@ -53,9 +54,9 @@ export class ProdutosComponent implements OnInit {
   }
 
   preencheFormularioEditar(produtoAtual: Produto){
-    console.log(produtoAtual.idproduto)
+    //console.log(produtoAtual.idproduto)
     this.id_produtoAtual = produtoAtual.idproduto;
-    console.log(this.id_produtoAtual)
+    //console.log(this.id_produtoAtual)
     this.formularioEditar.patchValue({
       titulo: produtoAtual.titulo,
       descricao: produtoAtual.descricao,
@@ -89,7 +90,7 @@ export class ProdutosComponent implements OnInit {
       this.produtos = res;
       for (let i = 0; i < this.produtos.length; i++) {
         this.produtoService.getFotosProduto(this.produtos[i].idproduto).subscribe(res => {
-          console.log("Produto id: "+this.produtos[i].idproduto)
+          //console.log("Produto id: "+this.produtos[i].idproduto)
           this.produtos[i].fotos = res;
           // console.log(this.produtos[i].fotos[0].data)
           
@@ -100,5 +101,17 @@ export class ProdutosComponent implements OnInit {
     }, err => {
       console.log(err);
     });
+  }
+
+  verificaLogin(){
+    
+    if(JSON.parse(localStorage.getItem('estaLogado'))){
+      console.log("Estou logado")
+      this.router.navigate(['/carrinho'])
+    }
+    else{
+      console.log("Não estou logado")
+      this.router.navigate(['/login'])
+    }
   }
 }
